8497da0659d9ff6e1379647d97ba805a68f0f27b
Benedikt Schwering <mail@bschwer.ing>
2025-01-14 09:30:38 +0100

    feat(frontend): reconfigure project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { Recipe } from './shared/models/recipe.model';
-import { RecipeService } from './core/services/recipe.service';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { LayoutComponent } from './layout/layout.component';
-import { LoginComponent } from './auth/login/login.component';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [LayoutComponent, CommonModule, HttpClientModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  providers: [RecipeService],
+  standalone: false,
+  styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'frontend';
-
-  public recipes: Recipe[] | undefined;
-
-  constructor(private recipeService: RecipeService) {}
-
-  ngOnInit() {
-    this.getRecipes();
-  }
-
-  public getRecipes(): void {
-    this.recipeService.getRecipes().subscribe(
-      (response: Recipe[]) => {
-        this.recipes = response;
-      },
-      (error: any) => {
-        console.error(error);
-      }
-    );
-  }
 }
--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { LoginComponent } from './auth/login/login.component';
-import { AppComponent } from './app.component';
-
-export const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: '', component: AppComponent },
-];
