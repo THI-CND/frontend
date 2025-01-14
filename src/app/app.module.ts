13781a3ba66f8e1b65015d0b785a37ef34b5ff75
Benedikt Schwering <mail@bschwer.ing>
2025-01-14 17:22:11 +0100

    feat(frontend): add project outline

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,56 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { RecipeComponent } from './components/recipe/recipe.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatDialogModule } from '@angular/material/dialog';
+import { CreateIngredientComponent } from './components/home/create-ingredient/create-ingredient.component';
+import { CreateRecipeComponent } from './components/home/create-recipe/create-recipe.component';
+import { RecipeStepComponent } from './components/recipe/recipe-step/recipe-step.component';
+import { RecipeStepQuantifiedIngredientComponent } from './components/recipe/recipe-step-quantified-ingredient/recipe-step-quantified-ingredient.component';
+import { EditRecipeComponent } from './components/recipe/edit-recipe/edit-recipe.component';
+import { MatSelectModule } from '@angular/material/select';
+import { CreateReviewComponent } from './components/recipe/create-review/create-review.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RecipeComponent,
+    CreateIngredientComponent,
+    CreateRecipeComponent,
+    RecipeStepComponent,
+    RecipeStepQuantifiedIngredientComponent,
+    EditRecipeComponent,
+    CreateReviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatChipsModule,
+    MatDialogModule,
+    MatSelectModule,
   ],
   providers: [
     provideAnimationsAsync()
