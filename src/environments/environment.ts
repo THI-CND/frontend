718d16cdab6b0775358228e0f7d688623685a9a9
Benedikt Schwering <mail@bschwer.ing>
2025-01-14 09:59:09 +0100

    feat(frontend): add proxy config and environments

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+export const environment = {
+    api: {
+        recipeV1: 'api/v1/recipe',
+        recipeV2: 'api/v2/recipe',
+
+        collectionV1: 'api/v1/collections',
+        collectionV2: 'api/v2/collections',
+
+        notificationV1: 'api/v1/notifications',
+        notificationV2: 'api/v2/notifications',
+
+        userV1: 'api/v1/users',
+
+        ingredientV1: 'api/v1/ingredients',
+        ingredientV2: 'api/v2/ingredients',
+
+        reviewV1: 'api/v1/reviews',
+
+        feedV1: 'api/v1/feed',
+
+        shoppinglistV1: 'api/v1/shopping-list',
+        shoppinglistV2: 'api/v2/shopping-list',
+    }
+};
